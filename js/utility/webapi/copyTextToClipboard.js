ceb7d71c462714a86d066045909b2a33517fa185
정서영 <jsyoung@planica.co.kr>
2018-12-07 15:09:59 +0900

    copyTextToClipboard

--- js/utility/webapi/copyTextToClipboard.js
+++ js/utility/webapi/copyTextToClipboard.js
@@ -1,4 +1,42 @@
+/**
+ * 클립보드에 텍스트 복사
+ * @param {string} textToCopy
+ */
 export function copyTextToClipboard(textToCopy = '') {
+  const hiddenInputId = 'hidden-input-for-copy'
+
+  const attachHiddenInput = () => {
+    const hiddenInputEl = document.createElement('input')
+
+    hiddenInputEl.style.position = 'fixed'
+    hiddenInputEl.style.bottom = '-100vh'
+    hiddenInputEl.type = 'text'
+    hiddenInputEl.id = hiddenInputId
+    document.getElementsByTagName('body')[0].appendChild(hiddenInputEl)
+
+    return hiddenInputEl
+  }
+
+  const getHiddenInputEl = () => {
+    const inputEl = document.getElementById(hiddenInputId)
+
+    if (!inputEl) {
+      return attachHiddenInput()
+    } else {
+      return inputEl
+    }
+  }
+
+  const copy = (el, str) => {
+    el.value = str
+    el.select()
+    document.execCommand('Copy')
+  }
+
+  copy(getHiddenInputEl(), textToCopy)
+}
+
+export function copyTextToClipboard_v1(textToCopy = '') {
   if (!canUseDOM()) {
     console.error('window is undefined')
   }
