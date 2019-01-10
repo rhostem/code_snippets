65aa0c4f876f4e388b0c0bc452a7b3fdf1e9c5ca
rhostem <syoung.j@gmail.com>
2019-01-10 21:49:13 +0900

    rename media query

--- js/react/style/media.js
+++ js/react/style/media.js
@@ -1,20 +1,22 @@
-import size from './size'
+/**
+ * 디바이스의 촤대 크기
+ */
+export const deviceMaxWidth = {
+  mobile: '480px',
+  tablet: '1024px',
+  desktop: '1920px',
+}
 
 const media = {
-  iphone5: `(max-width: 400px)`,
-  IPhone5: `@media (max-width: 400px)`,
-  mobile: `(min-width: 400px)`,
-  Mobile: `@media (min-width: 400px)`,
-  phablet: `(min-width: 550px)`,
-  Phablet: `@media (min-width: 550px)`,
-  tablet: `(min-width: 750px)`,
-  Tablet: `@media (min-width: 750px)`,
-  desktop: `(min-width: 1060px)`,
-  Desktop: `@media (min-width: 1060px)`,
-  hd: `(min-width: 1200px)`,
-  Hd: `@media (min-width: 1200px)`,
-  LargerThanAppMax: `@media (min-width: ${parseInt(size.APP_MAX_WIDTH, 10) +
-    1}px)`,
+  // media querys for mobile first
+  largerThanMobile: `(min-width: ${deviceMaxWidth.mobile})`,
+  largerThanTablet: `(min-width: ${deviceMaxWidth.tablet})`,
+  largerThanDesktop: `(min-width: ${deviceMaxWidth.desktop})`,
+
+  // media querys for desktop first.
+  smallerThanLargeDesktop: `(max-width: ${deviceMaxWidth.desktop})`,
+  smallerThanDesktop: `(max-width: ${deviceMaxWidth.tablet})`,
+  smallerThanTablet: `(max-width: ${deviceMaxWidth.mobile})`,
 }
 
 export default media
