652987bd4b6f9cc2f86dce1e490b46b92ec0b253
rhostem <syoung.j@gmail.com>
2019-05-07 12:44:02 +0900

    make unique id with randomString

--- js/utility/string/randomString.js
+++ js/utility/string/randomString.js
@@ -1,4 +1,4 @@
-export default function randomString(length = 8) {
+export default function randomString(length = 12) {
   let result = ''
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -7,5 +7,25 @@ export default function randomString(length = 8) {
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
+
   return result
+}
+
+export const makeId = (prefix = '_') => `${prefix}${makeRandomString()}`
+
+export const isUniqueId = (idMap = {}, newId = '') => !idMap[newId]
+
+export const getUniqueId = (idMap = {}, prefix = '_') => {
+  let newId = makeId(prefix)
+
+  while (true) {
+    if (isUniqueId(idMap, newId)) {
+      idMap[newId] = newId
+      break
+    } else {
+      newId = makeId(prefix)
+    }
+  }
+
+  return newId
 }
