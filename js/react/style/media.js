62b94bd498e79600b46725bfb225ac35c608328f
rhostem <syoung.j@gmail.com>
2019-06-19 00:39:08 +0900

    udpate media query mixin

--- js/react/style/media.js
+++ js/react/style/media.js
@@ -4,19 +4,22 @@
 export const deviceMaxWidth = {
   mobile: '480px',
   tablet: '1024px',
-  desktop: '1920px',
+  desktop: '1600px',
+  largeDesktop: '1920px',
 }
 
-const media = {
+export const media = {
+  iPhone5: `@media (max-width: 321px)`, // iPhone5보다 같거나 작은 사이즈에서 적용됨
+
   // media querys for mobile first
-  largerThanMobile: `(min-width: ${deviceMaxWidth.mobile})`,
-  largerThanTablet: `(min-width: ${deviceMaxWidth.tablet})`,
-  largerThanDesktop: `(min-width: ${deviceMaxWidth.desktop})`,
+  overMobile: `@media (min-width: ${deviceMaxWidth.mobile})`,
+  overTablet: `@media (min-width: ${deviceMaxWidth.tablet})`,
+  overDesktop: `@media (min-width: ${deviceMaxWidth.desktop})`,
 
   // media querys for desktop first.
-  smallerThanLargeDesktop: `(max-width: ${deviceMaxWidth.desktop})`,
-  smallerThanDesktop: `(max-width: ${deviceMaxWidth.tablet})`,
-  smallerThanTablet: `(max-width: ${deviceMaxWidth.mobile})`,
+  underLargeDesktop: `@media (max-width: ${deviceMaxWidth.largeDesktop})`,
+  underDesktop: `@media (max-width: ${deviceMaxWidth.desktop})`,
+  underTablet: `@media (max-width: ${deviceMaxWidth.tablet})`,
 }
 
 export default media
