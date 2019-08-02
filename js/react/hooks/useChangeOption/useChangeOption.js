50e815358083b11a67cc091028b6c02611dcdaf2
rhostem <syoung.j@gmail.com>
2019-08-02 17:59:13 +0900

    update useChangeOption

--- js/react/hooks/useChangeOption/useChangeOption.js
+++ js/react/hooks/useChangeOption/useChangeOption.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 /**
  * 옵션을 선택하는 컴포넌트에 사용할 수 있는 커스텀 훅
@@ -9,42 +9,37 @@ const useChangeOption = ({
   options = [], //
   initialValue,
 }) => {
-  const [value, setValue] = useState(initialValue)
-  const [label, setLabel] = useState('')
-  const [optionsAvailable, setOptionsVisible] = useState(options)
+  const [value, setValue] = useState(initialValue);
+  const [label, setLabel] = useState('');
+  const [optionsAvailable, setOptionsVisible] = useState(options);
 
-  const handleChange = option => {
-    setValue(option.value)
-    onChange(option.value)
-  }
+  const handleChange = value => {
+    setValue(value);
+  };
 
-  // 초기값 반영
+  // value 변경시 onChange 콜백 실행
   useEffect(() => {
-    setValue(initialValue)
-  }, [initialValue])
-
-  // 옵션이 변경되었을 때
-  useEffect(() => {
-    // 기본값 업데이트
-    const defaultValue = options && options.length > 0 ? options[0].value : null
-
-    setValue(defaultValue)
-    onChange(defaultValue)
-  }, [onChange, options])
-
-  // 선택 가능한 옵션
-  useEffect(() => {
-    // 선택된 값 제외
-    setOptionsVisible(options.filter(o => o.value !== value))
-  }, [options, value])
+    onChange(value);
+  }, [onChange, value]);
 
   // 라벨 업데이트
   useEffect(() => {
-    const target = options.find(o => o.value === value)
-    setLabel(target ? target.label : '')
-  }, [options, value])
+    const target = options.find(o => o.value === value);
+    setLabel(target ? target.label : '');
+  }, [options, value]);
 
-  return [value, label, handleChange, optionsAvailable]
-}
+  // 초기값 반영
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
 
-export default useChangeOption
+  // 선택 가능한 옵션 업데이트
+  useEffect(() => {
+    // 선택된 값 제외
+    setOptionsVisible(options.filter(o => o.value !== value));
+  }, [options, value]);
+
+  return [value, label, handleChange, optionsAvailable];
+};
+
+export default useChangeOption;
