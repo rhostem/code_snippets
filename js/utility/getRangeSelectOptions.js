0b3706fda621962e0c61193d1ee47654622af0f6
rhostem <syoung.j@gmail.com>
2019-09-08 12:49:43 +0900

    udpate getRangeSelectOptions

--- js/utility/getRangeSelectOptions.js
+++ js/utility/getRangeSelectOptions.js
@@ -1,37 +1,22 @@
-const sortAsc = (a, b) => a - b;
-const sortDesc = (a, b) => b - a;
-
 /**
  * Select 컴포넌트에 사용할 수 있도록 숫자를 값으로 가지는 객체 배열을 리턴한다.
  *
  * @param {*} option.start 시작
  * @param {*} option.end 종료
- * @param {*} option.sort  정렬. asc가 아니면 내림차순 정렬
- * @param {*} option.template 템플릿. %VALUE% 부분을 값으로 치환한다.
- * @param {*} option.templator 템플릿 함수. value를 받아서 문자열을 반환해야 한다. template 파라미터는 무시된다.
+ * @param {*} option.gap 값의 간격
+ * @param {*} option.mapFunc 배열에 실제로 입력할 값을 리턴하는 함수
  */
 export default function getRangeSelectOptions({
   start,
   end,
   gap = 1,
-  sort = "asc", // or DESC
-  template = `%VALUE%`,
-  templator
+  mapFunc = v => ({ value: v, label: v }),
 }) {
-  let options = [];
+  let options = []
+
   for (let i = start; i <= end; i += gap) {
-    options.push({
-      value: i,
-      label:
-        typeof templator === "function"
-          ? templator(i)
-          : template.replace(/%VALUE%/, i)
-    });
+    options.push(mapFunc(i))
   }
 
-  const sortFunc = sort === "asc" ? sortAsc : sortDesc;
-
-  options = options.sort((a, b) => sortFunc(a.value, b.value));
-
-  return options;
+  return options
 }
