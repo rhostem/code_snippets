4824f2af64ee6c23bd4eb70f6a3ef27200cbeb8a
rhostem <syoung.j@gmail.com>
2019-09-08 13:02:17 +0900

    fillArrayWithValue

--- js/utility/array/fillArrayWithValue.js
+++ js/utility/array/fillArrayWithValue.js
@@ -1,11 +1,13 @@
 /**
  * 원하는 값으로 채워진 배열을 만든다.
  *
- * @param {*} param.value 배열에 채워넣을 값
+ * @param {*} param.value 배열에 채워넣을 값, 함수 가능
  * @param {*} param.length 배열의 길이
  */
-const fillArrayWithValue = ({ value, length }) => {
-  return Array.from({ length }, () => value)
+const fillArrayWithValue = ({ mapper, length }) => {
+  return Array.from({ length }, (_, index) =>
+    typeof mapper === 'function' ? mapper(index) : mapper
+  )
 }
 
 export default fillArrayWithValue
