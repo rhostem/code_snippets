6ad3bf1087fe29106135ad53477f5c7f002f1f6b
rhostem <syoung.j@gmail.com>
2019-09-16 11:49:43 +0900

    upadte loadScript func

--- js/utility/webapi/loadScript.js
+++ js/utility/webapi/loadScript.js
@@ -1,13 +1,39 @@
 /**
- * 스크립트 로드
+ * script 태그로 외부 js 파일 불러오기
+ *
+ * @param {*} src 스크립트 URL
+ * @param {*} option.onload 스크립트가 로드된 후 실행할 콜백. 로드되었다면 바로 실행한다.
+ * @param {*} option.id script 태그 id
+ * @param {*} option.async 비동기 로드
  */
-export function loadScript(src, onload) {
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = src
-  script.onload = onload
+const loadScript = (
+  src = '',
+  { onLoad = () => {}, id, async = false } = {}
+) => {
+  const didNotLoaded = !document.getElementById(id);
 
-  const firstScript = document.getElementsByTagName('script')[0]
+  if (didNotLoaded) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.id = id || `script_${+new Date()}`;
+    script.async = async;
 
-  firstScript.parentNode.insertBefore(script, firstScript)
-}
+    script.onload = function() {
+      if (typeof onLoad === 'function') {
+        onLoad();
+      }
+    };
+
+    script.onerror = function() {
+      console.error('error on loading' + this.src);
+    };
+
+    document.body.appendChild(script);
+  } else {
+    if (typeof onLoad === 'function') {
+      onLoad();
+    }
+  }
+};
+
+export default loadScript;
