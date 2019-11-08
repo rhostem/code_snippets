98dd3a7f156cb0e161838ffffb3f34aaba0b0c33
rhostem <syoung.j@gmail.com>
2019-11-08 17:08:48 +0900

    update deploy git pm2

--- shell/deploy-git-pm2.sh
+++ shell/deploy-git-pm2.sh
@@ -1,10 +1,10 @@
 # 배포 스크립트
-APP_NAME=my-app-name
-HOST=1.1.1.1
-USER=username
-REPOSITORY=git@github.com:/reposutoryname.git
+APP_NAME=web-dev
+HOST=123.123.123.123
+USER=ec2-user
+REPOSITORY=git@github.com:username/reponame.git
 BRANCH=develop
-DEST_REPO=/home/username/www
+DEST_REPO=/home/ec2-user/reponame
 
-# 서버 접속 후 테스트 앱 시작
-ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git checkout $BRANCH && git reset --hard $BRANCH && git pull && npm install && npm run build && pm2 reload ecosystem.config.js --only $APP_NAME --env production"
+# # 서버 접속 후 테스트 앱 시작
+ssh -i ~/.ssh/guhada.pem $USER@$HOST "cd $DEST_REPO && git remote update --prune && git checkout $BRANCH && git reset --hard $BRANCH && git pull && npm install && npm run build && pm2 flush && pm2 reload ecosystem.config.js --only $APP_NAME --env production"
