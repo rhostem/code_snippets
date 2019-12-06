5fac81c8a39f1473c3d63c05c2ac4d40d1123c33
rhostem <syoung.j@gmail.com>
2019-12-06 12:53:10 +0900

    git subtree pull&push

---
+++ shell/git-subtree/pull.sh
@@ -0,0 +1,2 @@
+# git subtree pull  -P <prefix> <repository> <ref>
+git subtree pull --prefix childs/lib remote-lib master
---
+++ shell/git-subtree/push.sh
@@ -0,0 +1,2 @@
+# git subtree push  -P <prefix> <repository> <ref>
+git subtree push --prefix childs/lib remote-lib master
