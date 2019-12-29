5e2bac7364e01b493577d904adb128ee3a1a4f9a
rhostem <syoung.j@gmail.com>
2019-12-29 15:07:56 +0900

    getScrollPosition

--- js/utility/webapi/scroll.js
+++ js/utility/webapi/scroll.js
@@ -3,12 +3,29 @@ export const canUseDOM = () => typeof window !== 'undefined'
 export const checkIsMobile = () =>
   canUseDOM() && /mobile/i.test(window.navigator.userAgent)
 
+export const getScrollPosition = (el = window) => ({
+  x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
+  y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop,
+})
+
+export const isScrollable = () => {
+  if (canUseDOM()) {
+    const browserHeight = window.innerHeight // 뷰포트 높이
+    const documentHeight = document.documentElement.offsetHeight
+    const isScrollable = documentHeight > browserHeight
+
+    return isScrollable
+  } else {
+    return false
+  }
+}
+
 /**
  * 스크롤 방지
  * @param {*} isLock
  * @param {*} isLockTouchmove 터치 이벤트를 막는다. 모달에 스크롤되는 컨텐츠가 있을 경우 스크롤이 불가능해짐.
  */
-const setScrollability = ({
+export const setScrollability = ({
   isLockScroll = false,
   isLockTouchmove = false,
 }) => {
@@ -37,13 +54,12 @@ const setScrollability = ({
 
 /**
  * 스크롤 방지
- *
- * @export
  */
 export function disableScroll() {
+  // older FF
   if (window.addEventListener)
-    // older FF
     window.addEventListener('DOMMouseScroll', preventDefault, false)
+
   window.onwheel = preventDefault // modern standard
   window.onmousewheel = document.onmousewheel = preventDefault // older browsers, IE
   window.ontouchmove = preventDefault // mobile
@@ -52,26 +68,30 @@ export function disableScroll() {
 
 /**
  * 스크롤 가능
- *
- * @export
  */
 export function enableScroll() {
   if (window.removeEventListener)
     window.removeEventListener('DOMMouseScroll', preventDefault, false)
+
   window.onmousewheel = document.onmousewheel = null
   window.onwheel = null
   window.ontouchmove = null
   document.onkeydown = null
 }
 
-export const isScrollable = () => {
-  if (canUseDOM()) {
-    const browserHeight = window.innerHeight // 뷰포트 높이
-    const documentHeight = document.documentElement.offsetHeight
-    const isScrollable = documentHeight > browserHeight
+function preventDefault(e) {
+  e = e || window.event
+  if (e.preventDefault) e.preventDefault()
+  e.returnValue = false
+}
 
-    return isScrollable
-  } else {
+function preventDefaultForScrollKeys(e) {
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  var keyMakeScroll = { 37: 1, 38: 1, 39: 1, 40: 1 }
+
+  if (keyMakeScroll[e.keyCode]) {
+    preventDefault(e)
     return false
   }
 }
