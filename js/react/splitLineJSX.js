afc9aebed389e38dbeb47aef03e3672a51fe7ccd
rhostem <syoung.j@gmail.com>
2020-01-02 01:10:05 +0900

    update splitLineJSX (jsxTextLineBreak)

---
+++ js/react/splitLineJSX.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+/**
+ * 개행문자가 있는 문자열에 JSX br 태그를 추가해서 리턴한다
+ * @param {String} str 개행문자 포함된 문자열
+ */
+export default function splitLineJSX(str) {
+  const isLastIndex = (arr, i) => arr.length - 1 === i
+
+  return typeof str === 'string'
+    ? str.split(/\r?\n/).map((partial, i, partialArr) => (
+        <>
+          <span key={i}>{partial}</span>
+          {!isLastIndex(partialArr, i) && <br />}
+        </>
+      ))
+    : str
+}
