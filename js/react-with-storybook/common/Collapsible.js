3f24caefc310ccb8fe25d500aaa276946306f201
rhostem <syoung.j@gmail.com>
2020-04-10 11:33:46 +0900

    update collapsible

--- js/react-with-storybook/common/Collapsible.js
+++ js/react-with-storybook/common/Collapsible.js
@@ -2,12 +2,47 @@ import React, { useRef, useState, useEffect, useCallback, useMemo } from 'react'
 import styled from 'styled-components'
 
 const Wrap = styled.div``
+const TRANSIION_DELAY = 400
 
 const CollapsibleArea = styled.div`
   max-height: 0;
-  transition: max-height 0.2s linear;
+  transition: max-height ${TRANSIION_DELAY}ms ease-in-out;
   overflow: hidden;
 `
+
+export default function Collapsible({
+  isOpen = false,
+  renderHead = () => {},
+  children,
+}) {
+  const childrenRef = useRef(null)
+  const [childrenHeight, setChildrenHeight] = useState(0)
+
+  useEffect(() => {
+    if (isOpen) {
+      setChildrenHeight(childrenRef.current?.getBoundingClientRect().height)
+
+      setTimeout(() => {
+        // NOTE: 전환 효과가 끝났으면 충분한 높이를 줘서 중첩된 Collapsible에 문제가 없게 한다.
+        setChildrenHeight('200vh')
+      }, TRANSIION_DELAY)
+    } else {
+      setChildrenHeight(0)
+    }
+  }, [isOpen])
+
+  return (
+    <Wrap>
+      <div>{renderHead()}</div>
+      <CollapsibleArea
+        style={{
+          maxHeight: childrenHeight,
+        }}>
+        <div ref={childrenRef}>{children}</div>
+      </CollapsibleArea>
+    </Wrap>
+  )
+}
 
 export const useToggleOpen = (initialOpen = false) => {
   const [isOpen, setIsOpen] = useState(initialOpen)
@@ -23,27 +58,4 @@ export const useToggleOpen = (initialOpen = false) => {
     isOpen,
     toggleOpen,
   }
-}
-
-export default function Collapsible({
-  isOpen = true,
-  renderHead = () => {},
-  children,
-}) {
-  const childrenRef = useRef(null)
-  const childrenHeight = useMemo(() => {
-    const { height } = childrenRef.current?.getBoundingClientRect() || {}
-
-    return height
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [childrenRef.current])
-
-  return (
-    <>
-      <div>{renderHead()}</div>
-      <CollapsibleArea style={{ maxHeight: isOpen ? childrenHeight : 0 }}>
-        <div ref={childrenRef}>{children}</div>
-      </CollapsibleArea>
-    </>
-  )
 }
