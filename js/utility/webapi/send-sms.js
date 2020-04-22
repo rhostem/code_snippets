99e86df8fd3647744c52c539384b412bcfc84d76
rhostem <syoung.j@gmail.com>
2020-04-22 11:55:21 +0900

    send sms

--- js/utility/webapi/send-sms.js
+++ js/utility/webapi/send-sms.js
@@ -1,33 +1,33 @@
-const ua = window.navigator.userAgent
-const isMobile = /mobile/gi.test(ua)
+const isMobileNum = (num) => /^\d{3}-?\d{3,4}-?\d{4}$/.test(num)
 
-if (!isMobile) {
-  alert('모바일에서만 보낼 수 있습니다.')
-} else {
+/**
+ * 문자 메시지 보내기
+ */
+export default function sendSMS({ contents }) {
   let smsLink = 'sms://'
-  const { title, title_sub, code, owner_name } = this.state.eventData
-  const bodyText = `${title}\n${title_sub}\n${owner_name}\n\n${
-    process.env.PUBLIC_URL
-  }/event/${code}`
   const phone = window.prompt('메시지를 받을 휴대폰 번호를 입력하세요')
 
   if (isMobileNum(phone)) {
-    const isAndroid = /android/gi.test(ua)
-    if (isAndroid) {
-      smsLink = `sms://${phone}?body=${bodyText}`
-      window.open(encodeURI(smsLink))
-    } else {
-      const iosMatch = /iPhone OS (\d{1,2})/g.exec(ua)
-      const isIOS = iosMatch !== null
+    const ua = window.navigator.userAgent
+    const iosMatch = /iPhone OS (\d{1,2})/g.exec(ua)
+    const isIOS = iosMatch !== null
+    const phoneTrimmed = phone.replace(/-/g, '')
+
+    if (isIOS) {
       const iosVersion = isIOS ? parseInt(iosMatch[1], 10) : null
 
       if (iosVersion < 9) {
-        smsLink = `sms://${phone};body=${bodyText}`
+        smsLink = `sms://${phoneTrimmed};body=${contents}`
       } else {
-        smsLink = `sms://${phone}&body=${bodyText}`
+        smsLink = `sms://${phoneTrimmed}&body=${contents}`
       }
-      window.open(encodeURI(smsLink))
+    } else {
+      smsLink = `sms://${phoneTrimmed}?body=${contents}`
     }
+
+    console.log(`smsLink`, smsLink)
+
+    window.location.href = encodeURI(smsLink)
   } else {
     alert('유효한 휴대전화 번호가 아닙니다.')
   }
