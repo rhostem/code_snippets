6cbb5e3bd98a06cb3571b9bbb6dd06608bf5f24d
rhostem <syoung.j@gmail.com>
2020-04-27 11:08:02 +0900

    useImageLoader - use caching

--- js/react-with-storybook/hooks/useImageLoader.js
+++ js/react-with-storybook/hooks/useImageLoader.js
@@ -14,19 +14,41 @@ function arrayBufferToBase64(buffer) {
  *
  * @param {} imageUrl 원본 이미지
  */
-export default function useImageLoader(imageUrl) {
+export default function useImageLoader(imageUrl, { useImageStr = true } = {}) {
   const [imageUrlLoaded, setImageUrlLoaded] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const imageCacheMap = useRef(new Map())
+
+  const getCahcedImage = useCallback((img) => {
+    if (!!img && imageCacheMap.current.has(img)) {
+      return imageCacheMap.current.get(img)
+    }
+  }, [])
+
+  const setCachedImage = useCallback((key, data) => {
+    imageCacheMap.current.set(key, data)
+  }, [])
+
   const timer = useRef(null)
+
   const onFail = useCallback(() => {
-    // 실패하면 원본 이미지 url을 그대로 리턴하도록 한다.
+    // 실패하면 이미지 url을 그대로 리턴하도록 한다.
     setIsLoading(false)
     setImageUrlLoaded(imageUrl)
   }, [imageUrl])
 
+  const onLoadImage = useCallback(
+    (data) => {
+      setCachedImage(imageUrl, data)
+      setImageUrlLoaded(data)
+      setIsLoading(false)
+    },
+    [imageUrl, setCachedImage]
+  )
+
   useEffect(() => {
-    // 원본 이미지가 바뀌면 다음 이미지 로더를 위해 로딩된 이미지 초기화
     if (!!imageUrl) {
+      // 원본 이미지가 바뀌면 다음 이미지 로더를 위해 로딩된 이미지 초기화
       setImageUrlLoaded(null)
     }
   }, [imageUrl])
@@ -38,14 +60,23 @@ export default function useImageLoader(imageUrl) {
     timer.current = setTimeout(() => {
       if (!imageUrlLoaded) {
         setIsLoading(true)
+        console.log('delayed')
       }
     }, 300)
     return () => {}
   }, [imageUrlLoaded])
 
   useEffect(() => {
-    if (typeof window.fetch === 'function') {
-      if (imageUrl) {
+    if (!imageUrl) {
+      console.error('[useImageLoader] no image url')
+      return
+    } else if (typeof window.fetch === 'function') {
+      const cachedImage = getCahcedImage(imageUrl)
+
+      if (cachedImage) {
+        setImageUrlLoaded(cachedImage)
+        setIsLoading(false)
+      } else {
         var headers = new Headers({})
         var options = {
           method: 'GET',
@@ -57,29 +88,30 @@ export default function useImageLoader(imageUrl) {
 
         fetch(request, options)
           .then(async (response) => {
-            response
-              .arrayBuffer()
-              .then((buffer) => {
-                var base64Flag = 'data:image/jpeg;base64,'
-                var imageStr = arrayBufferToBase64(buffer)
+            try {
+              if (useImageStr) {
+                // NOTE: 이미지 데이터를 base64 문자열로 변환
+                const buffer = await response.arrayBuffer()
+                const base64Flag = 'data:image/jpeg;base64,'
+                const imageStr = arrayBufferToBase64(buffer)
                 const src = base64Flag + imageStr
-                setIsLoading(false)
-                setImageUrlLoaded(src)
-              })
-              .catch(() => {
-                onFail()
-              })
+                onLoadImage(src)
+              } else {
+                onLoadImage(imageUrl)
+              }
+            } catch (e) {
+              onFail()
+            }
           })
-          .catch(() => {
+          .catch((e) => {
             onFail()
           })
       }
     } else {
       // fetch API를 지원하지 않으면 이미지 로딩 처리하지 않음.
-      setImageUrlLoaded(imageUrl)
+      onLoadImage(imageUrl)
     }
-    return () => {}
-  }, [imageUrl, onFail])
+  }, [getCahcedImage, imageUrl, onFail, onLoadImage, useImageStr])
 
   return [imageUrlLoaded, isLoading]
 }
