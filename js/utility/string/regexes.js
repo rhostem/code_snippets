c872dc220628dec6202dbb64d4c884ec315abfce
rhostem <syoung.j@gmail.com>
2020-06-02 12:08:42 +0900

    showDecimalPointUnder

---
+++ js/utility/string/regexes.js
@@ -0,0 +1,4 @@
+/**
+ * YYYYMMDD_REGEX.test('2020-01-01') // true
+ */
+export const YYYYMMDD_REGEX = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/
---
+++ js/utility/string/showDecimalPointUnder.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {*} value 숫자. 실수
+ * @param {*} point 표시할 소수점 자리
+ */
+export default function showDecimalPointUnder(value, point = 0) {
+  if (value && value !== 0 && typeof value === 'number') {
+    if (point === 0) {
+      return parseInt(value)
+    } else {
+      return parseInt(value * Math.pow(10, point)) / Math.pow(10, point)
+    }
+  } else {
+    return value
+  }
+}
