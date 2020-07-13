43bd2fe2d543281be991c9b27a764e86642d715c
rhostem <syoung.j@gmail.com>
2020-07-13 19:12:33 +0900

    fix omit decimals

--- js/utility/string/omitDecimals.test.js
+++ js/utility/string/omitDecimals.test.js
@@ -3,15 +3,23 @@ import omitZeroDecimals from './omitZeroDecimals'
 describe('omitZeroDecimals는', () => {
   it('소수점 이하 자리가 모두 0이면 정수로 변환한다. ', () => {
     expect(omitZeroDecimals(12.0)).toEqual(12)
+    expect(omitZeroDecimals('12.0')).toEqual(12)
     expect(omitZeroDecimals(-12.0)).toEqual(-12)
+    expect(omitZeroDecimals('-12.0')).toEqual(-12)
   })
 
   it('소수점 이하가 모두 0이 아니면 아니면 그대로 반환한다', () => {
     expect(omitZeroDecimals(12.5)).toEqual(12.5)
+    expect(omitZeroDecimals('12.5')).toEqual(12.5)
     expect(omitZeroDecimals(-12.5)).toEqual(-12.5)
+    expect(omitZeroDecimals('-12.5')).toEqual(-12.5)
   })
 
   it('0은 그대로', () => {
     expect(omitZeroDecimals(0.0)).toEqual(0)
+  })
+
+  it('숫자가 아닌 문자열은 그대로', () => {
+    expect(omitZeroDecimals('abs')).toBe('abs')
   })
 })
--- js/utility/string/omitZeroDecimals.js
+++ js/utility/string/omitZeroDecimals.js
@@ -2,12 +2,22 @@
  * 소수점 이하 자리가 모두 0이면 정수로 변환한다.
  */
 export default function omitZeroDecimals(num) {
-  if (typeof num === 'number') {
-    const sign = num > 0 ? 1 : -1
-    const isZero = num === 0
-    const isAllZeroUnderDecimal = Math.abs(num) - parseInt(num, 10) === 0
-    return isZero ? 0 : isAllZeroUnderDecimal ? parseInt(num) * sign : num
+  if (!isNaN(parseFloat(num))) {
+    const floatNum = parseFloat(num)
+    const sign = floatNum > 0 ? 1 : -1
+    const isZero = floatNum === 0
+    const isAllZeroUnderDecimal =
+      Math.abs(floatNum) - parseInt(floatNum, 10) === 0
+
+    return isZero
+      ? 0
+      : isAllZeroUnderDecimal
+      ? parseInt(floatNum) * sign
+      : floatNum
   } else {
-    console.error(`[omitZeroDecimals] parameter should be a number`)
+    console.warn(
+      `[omitZeroDecimals] parameter should be a number or number string`
+    )
+    return num
   }
 }
