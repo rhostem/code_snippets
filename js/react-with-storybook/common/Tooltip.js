2207dcd7439ae0b8bdcc41e89580dd61509313ab
rhostem <syoung.j@gmail.com>
2020-07-16 15:00:35 +0900

    update Tooltip style

--- js/react-with-storybook/common/Tooltip.js
+++ js/react-with-storybook/common/Tooltip.js
@@ -4,22 +4,21 @@ import styled from 'styled-components'
 const Wrap = styled.div`
   position: absolute;
   z-index: 10;
-  left: 50%;
-  top: 50%;
-  padding: 2em;
+  left: 0;
+  top: calc(100% + 1em);
+  width: 400px;
+  height: auto;
+  padding: 16px;
+  font-size: 14px;
   border-radius: 10px;
   background: #fff;
   box-shadow: 10px 10px 30px 0 rgba(76, 103, 140, 0.2);
-  width: 400px;
-  height: auto;
 `
 
 export default function Tooltip({ isOpen, wrapperCSS, style, children }) {
-  return (
-    isOpen && (
-      <Wrap css={wrapperCSS} style={style}>
-        {children}
-      </Wrap>
-    )
-  )
+  return isOpen ? (
+    <Wrap css={wrapperCSS} style={style}>
+      {children}
+    </Wrap>
+  ) : null
 }
