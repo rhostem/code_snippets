bbac1c1c3c0043aa9ceaa7a6ce4c2247dfaf89a7
rhostem <syoung.j@gmail.com>
2021-02-11 00:52:48 +0900

    update typechec

--- js/utility/object/typeCheck.ts
+++ js/utility/object/typeCheck.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-types */
 export const isNumber = (v: any): v is number => typeof v === 'number'
+
 export const isString = (v: any): v is string => typeof v === 'string'
+
 export const isBoolean = (v: any): v is boolean => typeof v === 'boolean'
+
 export const isFunction = (v: any): v is Function => typeof v === 'function'
-export const isArray = <T>(v: any): v is T[] => v.constructor === Array
+
+export const isArray = <T>(v: any): v is T[] => v && v.constructor === Array
+
 export const isObject = <T extends { [key: string]: boolean }>(
   v: any
-): v is T => v.constructor === Object
+): v is T => v && v.constructor === Object
+
+export const isDate = (v: any): v is Date => v && v.constructor === Date
