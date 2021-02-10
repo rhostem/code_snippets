5d5a5ec9a8807b397281390f2d31c8669b5f20da
rhostem <syoung.j@gmail.com>
2021-02-11 00:08:51 +0900

    update typeCheck

--- js/utility/object/typeCheck.ts
+++ js/utility/object/typeCheck.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-types */
 export const isNumber = (v: any): v is number => typeof v === 'number'
 export const isString = (v: any): v is string => typeof v === 'string'
 export const isBoolean = (v: any): v is boolean => typeof v === 'boolean'
 export const isFunction = (v: any): v is Function => typeof v === 'function'
 export const isArray = <T>(v: any): v is T[] => v.constructor === Array
+export const isObject = <T extends { [key: string]: boolean }>(
+  v: any
+): v is T => v.constructor === Object
