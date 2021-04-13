3f47344fdfa806fb658a58adbc870e18608f8b15
rhostem <syoung.j@gmail.com>
2021-04-13 18:51:44 +0900

    hextToRgba

---
+++ js/utility/etc/hextToRgba.js
@@ -0,0 +1,33 @@
+/**
+ * ECMA2015
+ */
+function convertHex(hexCode, opacity) {
+  var hex = hexCode.replace('#', '')
+
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+  }
+
+  var r = parseInt(hex.substring(0, 2), 16),
+    g = parseInt(hex.substring(2, 4), 16),
+    b = parseInt(hex.substring(4, 6), 16)
+
+  return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')'
+}
+
+/**
+ * ECMA2016 / ES6
+ */
+const convertHexToRGBA = (hexCode, opacity) => {
+  let hex = hexCode.replace('#', '')
+
+  if (hex.length === 3) {
+    hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`
+  }
+
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+
+  return `rgba(${r},${g},${b},${opacity / 100})`
+}
