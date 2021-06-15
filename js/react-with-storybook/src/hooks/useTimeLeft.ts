b85409238da33282aa49601b8d93aa98ef19d431
rhostem <syoung.j@gmail.com>
2021-06-15 18:30:05 +0900

    useTimeLeft

--- js/react-with-storybook/src/hooks/useTimeLeft.ts
+++ js/react-with-storybook/src/hooks/useTimeLeft.ts
@@ -1,74 +1,74 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react';
 
-export const TIMELEFT_TEXT_BEFORE_START = '--:--'
+export const TIMELEFT_TEXT_BEFORE_START = '--:--';
 
-type VoidCallback = () => void
+type VoidCallback = () => void;
 
 type HookReturns = {
-  left: number
-  formatTimeLeft: () => string
-  startTimer: (limit: number, callback: VoidCallback) => void
-  resetTimer: VoidCallback
-}
+  left: number;
+  formatTimeLeft: () => string;
+  startTimer: (limit: number, callback: VoidCallback) => void;
+  resetTimer: VoidCallback;
+};
 
 const useTimeLeft = (): HookReturns => {
-  const [left, setLeft] = useState<number>(-1)
-  const timer = useRef<ReturnType<typeof setInterval> | null>(null)
+  const [left, setLeft] = useState<number>(-1);
+  const timer = useRef<ReturnType<typeof setInterval> | null>(null);
 
   const clearTimerInterval = useCallback(() => {
     if (timer.current) {
-      clearInterval(timer.current)
+      clearInterval(timer.current);
     }
-  }, [])
+  }, []);
 
   const resetTimer = useCallback(() => {
-    clearTimerInterval()
-    setLeft(-1)
-  }, [clearTimerInterval])
+    clearTimerInterval();
+    setLeft(-1);
+  }, [clearTimerInterval]);
 
   const startTimer = useCallback(
     (limit: number, onTimeout: VoidCallback) => {
-      resetTimer()
+      resetTimer();
 
       if (limit > 0) {
-        let timeLeft = limit
-        setLeft(timeLeft)
+        let timeLeft = limit;
+        setLeft(timeLeft);
 
         timer.current = setInterval(() => {
-          timeLeft -= 1
-          setLeft(timeLeft)
+          timeLeft -= 1;
+          setLeft(timeLeft);
 
           if (timeLeft === 0 && onTimeout) {
-            onTimeout()
-            clearTimerInterval()
+            onTimeout();
+            clearTimerInterval();
           }
-        }, 1000)
+        }, 1000);
       }
     },
     [clearTimerInterval, resetTimer]
-  )
+  );
 
   useEffect(
     () => () => {
       // clear timer on unmount of component
-      resetTimer()
+      resetTimer();
     },
     [resetTimer]
-  )
+  );
 
   const formatTimeLeft = useCallback(() => {
-    const hour = Math.floor(left / 3600)
+    const hour = Math.floor(left / 3600);
     const minute = Math.floor((left % 3600) / 60)
       .toString()
-      .padStart(2, '0')
-    const second = (left % 60).toString().padStart(2, '0')
+      .padStart(2, '0');
+    const second = (left % 60).toString().padStart(2, '0');
 
     return left >= 0
       ? `${hour > 0 ? `${hour}:` : ''}${minute}:${second}`
-      : TIMELEFT_TEXT_BEFORE_START
-  }, [left])
+      : TIMELEFT_TEXT_BEFORE_START;
+  }, [left]);
 
-  return { left, startTimer, resetTimer, formatTimeLeft }
-}
+  return { left, startTimer, resetTimer, formatTimeLeft };
+};
 
-export default useTimeLeft
+export default useTimeLeft;
